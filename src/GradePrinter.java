7e8c47adb68d7de928698cc9074a3426822f2fe6
playrey2010 <playrey2010@gmail.com>
2019-09-04 20:06:25 -0400

    if else instead of switch

--- src/GradePrinter.java
+++ src/GradePrinter.java
@@ -2,71 +2,48 @@ import java.util.Scanner;
 public class GradePrinter {
 
     public static void main(String[] args) {
-        int score;
+        int score = 0;
         String grade = "";
-        boolean again = true;
+        String again = "y";
 
 
         Scanner sc = new Scanner(System.in);
-        while (again) {
-            System.out.println("Enter an exam score");
+        while (again.equalsIgnoreCase("y")) {
+            System.out.print("Enter an exam score: ");
             score = sc.nextInt();
 
-            switch (score/10) {
-                case 10:
-                case 9:
-                    grade = "A";
-                    break;
-                case 8:
-                    grade = "B";
-                    break;
-                case 7:
-                    grade = "C";
-                    break;
-                case 6:
-                    grade = "D";
-                    break;
-                case 5:
-                    grade = "F";
-                    break;
-                default:
-                    grade = "Z";
-                    break;
+            if (score >= 97 &&  score <= 100) {
+                grade = "A+";
+            } else if (score >= 94 &&  score <= 96) {
+                grade = "A";
+            } else if (score >= 90 &&  score <= 93) {
+                grade = "A-";
+            } else if (score >= 87 &&  score <= 89) {
+                grade = "B+";
+            } else if (score >= 84 &&  score <= 86) {
+                grade = "B";
+            } else if (score >= 80 &&  score <= 83) {
+                grade = "B-";
+            } else if (score >= 77 &&  score <= 79) {
+                grade = "C+";
+            } else if (score >= 74 &&  score <= 76) {
+                grade = "C";
+            } else if (score >= 70 &&  score <= 73) {
+                grade = "C-";
+            } else if (score >= 60 &&  score <= 69) {
+                grade = "D";
             }
-            if (grade.equals("Z") || grade.equals("F")) {
-                System.out.println("Go back to school");
-            } else {
-                System.out.println("Good job. Your grade is " + grade);
-                System.out.println("Do you want to enter another score (Y/N)?");
 
+            if (!(score < 60)) {
+                System.out.println("Good job. Your grade is " + grade + ".");
+                System.out.println("Do you want to enter another score?");
+                again = sc.next();
+            } else if (score<60) {
+                System.out.println("Go back to school.");
+                System.out.println("Do you want to enter another score (Y/N)?");
+                again = sc.next();
             }
         }
-//        System.out.println("Enter an exam score");
-//        score = sc.nextInt();
-//
-//        switch (score/10) {
-//            case 10:
-//            case 9:
-//                grade = "A";
-//                break;
-//            case 8:
-//                grade = "B";
-//                break;
-//            case 7:
-//                grade = "C";
-//                break;
-//            case 6:
-//                grade = "D";
-//                break;
-//            case 5:
-//                grade = "F";
-//                break;
-//            default:
-//                grade = "Z";
-//                break;
-//        }
-
-        System.out.println(grade);
     }
 
 }
